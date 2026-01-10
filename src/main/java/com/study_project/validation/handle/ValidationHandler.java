f2346481db1df182a800b706f90cd22fb6b7238c
Nechelley <Nechelley@live.com>
2026-01-10 11:25:01 -0300

    Fixing validation handle

--- src/main/java/com/study_project/validation/handle/ValidationHandler.java
+++ src/main/java/com/study_project/validation/handle/ValidationHandler.java
@@ -1,7 +1,8 @@
 package com.study_project.validation.handle;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,15 +14,10 @@ import com.study_project.validation.dto.GenericFieldErrorDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @RestControllerAdvice
 public class ValidationHandler {
-
-	private final MessageSource messageSource;
-
-	public ValidationHandler(MessageSource messageSource) {
-		this.messageSource = messageSource;
-	}
 
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -30,8 +26,24 @@ public class ValidationHandler {
 
 		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 		fieldErrors.forEach(error -> {
-			String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
-			formErrors.add(new GenericFieldErrorDto(error.getField(), message));
+			String field = error.getField();
+			String message = error.getDefaultMessage();
+			formErrors.add(new GenericFieldErrorDto(field, message));
+		});
+
+		return formErrors;
+	}
+
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public List<GenericFieldErrorDto> handle(ConstraintViolationException exception) {
+		List<GenericFieldErrorDto> formErrors = new ArrayList<>();
+
+		Set<ConstraintViolation<?>> fieldErrors = exception.getConstraintViolations();
+		fieldErrors.forEach(error -> {
+			String field = error.getPropertyPath().toString();
+			String message = error.getMessage();
+			formErrors.add(new GenericFieldErrorDto(field.substring(field.lastIndexOf('.') + 1), message));
 		});
 
 		return formErrors;
